84cdc860b91e4c0822a5d648a49c95cca0809065
Omegattr <ttrkoza904@gmail.com>
2017-12-18 21:57:40 +0900

    オブジェクト指向4Pageまで

---
+++ TestProject01/src/HelloWorld.java
@@ -0,0 +1,48 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		/*for(int i = 1; i <= 100; i++){
+			System.out.println(i);
+			System.out.println("-");
+		}*/
+
+		/*for(int i=1;i<=5;i++){
+			System.out.println("★");
+		}*/
+
+		/*for(int a=0; a<3; a++){
+			for(int b=1;b<4;b++){
+				System.out.println(b);
+			}
+			System.out.println("\n");
+		}*/
+
+		/*int a=2;
+		String b=a%2==0 ?"偶数":"奇数";
+		System.out.println(b);*/
+
+		/*for(int i=0;i<=5;i++){
+			String b=i%2==0?"☆":"★";
+			System.out.println(b);
+		}*/
+
+		/*int i=1;
+		while(i<=5){
+			System.out.println("★");
+			i++;
+		}*/
+
+		int i=5;
+		do{
+			System.out.println(i);
+			i+=2;
+		} while(i<=10);
+
+
+
+	}
+
+}
